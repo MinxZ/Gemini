a9015769a360e7b1b116f01c5bdca97725001da6
MinxZ <z670172581@icloud.com>
2023-01-05 10:59:01 -0800

    Fix get_data script

--- get_data.sh
+++ get_data.sh
@@ -9,25 +9,23 @@ tar -xvf mashup.tar.gz
 mv data/networks/* ..
 cd ..
 wget https://stringdb-static.org/download/protein.links.detailed.v11.5/10090.protein.links.detailed.v11.5.txt.gz
+gunzip 10090.protein.links.detailed.v11.5.txt
+cd ..
 
 # aliase
-cd ..
 mkdir aliase
 cd aliase
 wget https://stringdb-static.org/download/protein.aliases.v11.5/9606.protein.aliases.v11.5.txt.gz
 wget https://stringdb-static.org/download/protein.aliases.v11.5/10090.protein.aliases.v11.5.txt.gz
 gunzip 9606.protein.aliases.v11.5.txt
 gunzip 10090.protein.aliases.v11.5.txt
+cd ..
 
 # BioGRID
 for org in Homo_sapiens Mus_musculus Saccharomyces_cerevisiae; do
-    cd ..
-    mv $org ${org}_all
-    cd ${org}_all
-    mv * ..
-    cd ..
-    rm -r ${org}_all
+    unzip ${org}.zip
 done
 
 # GOA
+unzip GOA.zip
 mv GOA goa
